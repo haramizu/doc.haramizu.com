8a4fdd9619f96e877935f9728b01d6bcf81e4064
Shinichi Haramizu <shinichi@haramizu.jp>
2024-11-11 18:01:12 +0900

    add gtm tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
 
+const GTM_ID = process.env.GTM_ID;
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://doc.haramizu.com',
@@ -10,6 +12,25 @@ export default defineConfig({
     starlight({
       title: 'Haramizu.com',
       defaultLocale: 'root',
+      head: [
+        {
+          tag: 'script',
+          attrs: {
+            async: true,
+            src: `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`,
+          },
+        },
+        {
+          tag: 'script',
+          content: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        },
+      ],
       locales: {
         root: {
           label: '日本語',
