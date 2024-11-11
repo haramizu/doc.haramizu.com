4d5e7893a73a09c687b574775c36c52a76482174
Shinichi Haramizu <shinichi@haramizu.jp>
2024-11-11 18:02:10 +0900

    add google tag manager

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,65 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+const GTM_ID = process.env.GTM_ID;
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://doc.haramizu.com",
+  site: 'https://doc.haramizu.com',
   integrations: [
     starlight({
-      title: "Shinichi Haramizu",
-      defaultLocale: "root",
+      title: 'Shinichi Haramizu',
+      defaultLocale: 'root',
       locales: {
         root: {
-          label: "日本語",
-          lang: "ja",
+          label: '日本語',
+          lang: 'ja',
         },
         en: {
-          label: "English",
-          lang: "en",
+          label: 'English',
+          lang: 'en',
         },
       },
       social: {
-        github: "https://github.com/withastro/starlight",
+        github: 'https://github.com/withastro/starlight',
       },
       sidebar: [
         {
-          label: "Guides",
+          label: 'Guides',
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
+            { label: 'Example Guide', slug: 'guides/example' },
           ],
         },
         {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
         },
       ],
-      customCss: ["./src/tailwind.css"],
+      customCss: ['./src/tailwind.css'],
+      head: [
+        {
+          tag: 'script',
+          attrs: {
+            async: true,
+            src: `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`,
+          },
+        },
+        {
+          tag: 'script',
+          content: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        },
+      ],
     }),
     tailwind({ applyBaseStyles: false }),
   ],
 });
+
